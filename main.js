58e686792f2d521274ddaf4306c4941ab3a8ee7a
navanacho <navarria333@gmail.com>
2025-10-24 23:27:00 -0300

    modificaciones varias

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
-function toggleSkills(id){
-    const element = document.getElementById(id);
-    const allIcons = document.querySelectorAll('skill-icon');
+const sections = document.getBoundingClientRect().top;
+ 
+window.addEventListener('scroll', () => {
+    sections.forEach(section => {
+        const sectionTop = document.getBoundingClientRect().top;
+        const sectionVisible = sectionTop < window.innerHeight - 100;
 
-    allIcons.forEach(el =>{
-        if (el.id !== id){
-            el.classList.add('hidden');
-        }
-    });
-
-    element.classList.toggle('hidden');
-}
-
-//animacion del scroller
-const sections = document.querySelectorAll('section');
-
-window.addEventListener('scroll', () =>{
-    sections.forEach(section =>{
-        const sectionTop = section.getBoundingClientRect().top;
-        const sectionHeight = section.clientHeight;
-
-        if (sectionTop < window.innerHeight - 100 && sectionTop > -sectionHeight){
+        if(sectionVisible){
             section.classList.add('visible');
         }
     });
 });
+
+window.addEventListener('load', () =>{
+    setTimeout(() =>{
+        sections.forEach(sectio =>{
+            const sectionTop = section.getBoundingClientRect().top;
+            if(sectionTop < window.innerHeight - 100){
+                section.classList.add('visible');
+            }
+        });
+    }, 100);
+})
